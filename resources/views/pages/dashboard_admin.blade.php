dae44650afd42742624e39dfc131927604cedc5d
Chelsea Maharani <putrichelseamaharani@gmail.com>
2025-07-19 19:04:53 +0700

    admin

--- resources/views/pages/dashboard_admin.blade.php
+++ resources/views/pages/dashboard_admin.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('title', 'Dashboard Admin')
+
 @if(session('logged_in_user') && session('logged_in_user')->role === 'user')
   <script>
     window.location.href = "/landing_page1";
@@ -12,6 +13,7 @@
     <div class="bg-primary text-secondary rounded-3xl p-7 min-h-screen lg:min-h-[560px] flex-1">
         <h4 class="text-xl sm:text-2xl mb-1 mt-1">Hello, Admin!</h4>
         <p class="text-base sm:text-lg">Welcome to your dashboard.</p>
+
         <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-10">
             @include('components.card_admin', [
                 'title' => 'Room Data',
@@ -27,6 +29,7 @@
                 'link' => 'orders_total_admin',
             ])
         </div>
+
         <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-7">
             @include('components.card_admin', [
                 'title' => 'User Data',
@@ -43,10 +46,4 @@
             ])
         </div>
     </div>
-
-    @if (session('success'))
-        <script>
-            alert("{{ session('success') }}");
-        </script>
-    @endif
 @endsection
